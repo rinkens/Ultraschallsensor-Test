e5be45f10e3dd9eb4fce35228e3e1891e94202c1
Tim Rinkens <50915685+rinkens@users.noreply.github.com>
2023-07-27 18:12:32 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,13 +5,13 @@ basic.forever(function () {
         music.ringTone(523)
         basic.showIcon(IconNames.No)
     } else {
+        basic.clearScreen()
         if (entfernung < 120) {
-            music.playTone(262, music.beat(BeatFraction.Eighth))
-            basic.pause(entfernung * 5)
-            basic.clearScreen()
             for (let Index = 0; Index <= 4 - Math.round(entfernung / 120 * 5); Index++) {
                 led.plot(0, Index)
             }
+            music.playTone(262, music.beat(BeatFraction.Eighth))
+            basic.pause(entfernung * 5)
         }
     }
 })
